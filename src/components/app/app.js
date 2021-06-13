9ea5fd18d6aa7aa7814a20cd2fb5787d5adcfac2
zakhariak <azakhariak@gmail.com>
2021-06-13 21:35:12 +0300

    creation weatherService , connection API

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,16 @@
 import React from "react";
 import CityWeather from "../city-weather";
 import Header from "../header";
+import WeatherService from "../../services/weather-servise";
 
 import "./app.scss";
 
 const App = () => {
+  const { getWeatherInCity, getDataWeatherByDay } = new WeatherService();
+  getWeatherInCity("paris").then((data) => {
+    console.log(getDataWeatherByDay(data.weatherByDays[0]));
+  });
+
   return (
     <div>
       <Header />
---
+++ src/services/weather-servise.js
@@ -0,0 +1,60 @@
+export default class WeatherService {
+  _apiBase = "https://api.openweathermap.org/data/2.5";
+  _apiKey = "2ae10c36fa338f88184fbc2160af3867";
+
+  async getResource(city) {
+    const res = await fetch(
+      `${this._apiBase}/forecast?q=${city}&appid=${this._apiKey}`
+    );
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${city}, received ${res.status}`);
+    }
+    return await res.json();
+  }
+
+  getWeatherInCity = async (city) => {
+    const data = await this.getResource(city);
+    return this._transformDataWeather(data);
+  };
+
+  getDataWeatherByDay = async (weatherDataByDay) => {
+    return this._transformCityWeatherByDay(weatherDataByDay);
+  };
+
+  _transformDataWeather = (weatherData) => {
+    return {
+      name: weatherData.city.name,
+      country: weatherData.city.country,
+      weatherByDays: weatherData.list,
+    };
+  };
+
+  _transformCityWeatherByDay = (weatherByDay) => {
+    console.log(weatherByDay);
+    const { clouds, main, weather, wind, dt_txt } = weatherByDay;
+    const daysName = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const date = new Date(dt_txt);
+    return {
+      date: {
+        day: daysName[date.getDay()],
+        date: date.getDate(),
+        hour: new Date().getHours(),
+        minutes: new Date().getMinutes(),
+      },
+      cloudliness: clouds.all,
+      humidity: main.humidity,
+      plessure: main.humidity,
+      temp: main.temp,
+      weatherDescription: weather[0].description,
+      wind: wind.speed,
+    };
+  };
+}
